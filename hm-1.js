b84b0fda03eb4ddd6993a6214f3447d6c1e10eaf
KuzinVitaliy <vkuzin@vk-soft.ru>
2024-09-18 13:23:19 +0300

    Done

---
+++ hm-1.js
@@ -0,0 +1,54 @@
+return;
+
+// Task 01
+let a = 10;
+alert(`A=${a}`);
+a = 20;
+alert(`A=${a}`);
+
+// Task 02
+let firstIPhoneYear = 2007;
+alert(`Год выпуска первого iPhone - ${firstIPhoneYear}`);
+
+// Task 03
+let javaScriptCreator = "Брендан Эйх";
+alert(`Создателя JavaScript зовут ${javaScriptCreator}`);
+
+// Task 04
+a = 10;
+let b = 2;
+alert(`Сумма a и b равна ${a + b}`);
+alert(`Разность a и b равна ${a - b}`);
+alert(`Произведение a и b равно ${a * b}`);
+alert(`Частное a и b равно ${a / b}`);
+
+// Task 05
+let result = 2 ** 5;
+alert(`2**5 равно ${result}`);
+
+// Task 06
+a = 9;
+b = 2;
+result = a % b;
+alert(`Остаток от деления ${a} на ${b} равен ${result}`);
+
+// Task 07
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Task 08
+let age = Number(prompt("Сколько Вам лет"));
+alert(`Пользователю ${age} лет`);
+
+// Task 09
+let user = { name: 'Иванов Иван', age: 34, isAdmin: true }
+
+// Task 10
+let userName = prompt("Как вас зовут?");
+alert(`Привет, ${userName}!`);
