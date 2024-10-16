48979992b7f1ce9120fb422da089ec2de0c3d413
KuzinVitaliy <vkuzin@vk-soft.ru>
2024-10-16 22:21:52 +0300

    Predone

---
+++ game.js
@@ -0,0 +1,20 @@
+function GotoGame(gameNumber) {
+    switch (gameNumber) {
+        case 1:
+            window.location.href = './game01.html'
+            break;
+        case 2:
+
+            break;
+        case 3:
+            window.location.href = './game03.html'
+            break;
+        case 4:
+            window.location.href = './game04.html'
+            break;
+
+        default:
+            break;
+    }
+
+}
