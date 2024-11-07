207b456a9074d7aebe92592e76f9fd65fb75a3c4
KuzinVitaliy <vkuzin@vk-soft.ru>
2024-11-07 09:27:43 +0300

    Done

--- js/game06.js
+++ js/game06.js
@@ -1,16 +1,42 @@
 function RandomColor() {
     const listEl = document.getElementById("colorRegion");
-    listEl.style.backgroundColor = Math.RandomColor;
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    const rntClr = `rgb(${r} ${g} ${b})`;
+    listEl.style.backgroundColor = rntClr;
 }
 
 
+let r, g, b;
 function RandomColor2() {
-    const listEl = document.getElementById("colorRegion");
-    while (1 == 1) {
-        let a = Math.random() * 3;
-        let b = Math.random() * 10 - 20;
-        let clr = listEl.backgroundColor;
-        // clr.
-    }
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    setInterval(Transitions, 100);
+}
 
+function Transitions() {
+    const listEl = document.getElementById("colorRegion");
+    let a = Math.round(Math.random() * 2);
+    switch (a) {
+        case 0: r = DeltaColor(r);
+            break;
+        case 1: g = DeltaColor(g);
+            break;
+        case 2: b = DeltaColor(b);
+            break;
+    }
+    const rntClr = `rgb(${r} ${g} ${b})`;
+    listEl.style.backgroundColor = rntClr;
+}
+
+
+function DeltaColor(a) {
+    const delSize = 20;
+    let delta = Math.round(Math.random() * delSize * 2) - delSize;
+    a += delta;
+    if (a < 0) a = 0;
+    if (a > 255) a = 255;
+    return a;
 }
